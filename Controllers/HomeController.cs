8b8fe0b71ec325544a8f62d259bacba60ec3fb0c
ssssasha <sashyniaor@gmail.com>
2021-05-28 23:46:04 +0300

    Add all my files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello world!";
+            return View("Index");
+        }
+    }
+}
